6c93ba3e9a837d251806ed0f3cd2f2796eb013f1
fatimzahra1 <fatimzahraelmadini@gmail.com>
2021-03-09 14:41:32 +0100

    --- Auto-Git Commit ---

--- includes/registration.php
+++ includes/registration.php
@@ -5,9 +5,47 @@
   <body>
 
   <div>
-      <form action="register.php" method="post">
+      <?php 
+      if ( isset ($_POST['create'] ) ) {
+          echo 'User submitted.'; }
+        ?>
+  </div>
+
+  <div>
+      <form action="registration.php" method="post">
           <div class="container">
               <h1>Registration</h1>
+              <p>Fill in this form to registrate with us</p>
+
+              <!--FOR FIRST NAME-->
+
+              <label for="firstname"><b>First name</b></label>
+              <input type="text" name="firstname" required>
+
+               <!--FOR LAST NAME-->
+              
+               <label for="lastname"><b>Last name</b></label>
+              <input type="text" name="lastname" required>
+
+               <!--FOR EMAIL-->
+              
+               <label for="email"><b>Email adress</b></label>
+              <input type="email" name="email" required>
+
+               <!--FOR PHONE NUMBER-->
+              
+               <label for="phonenumber"><b>Phone number</b></label>
+              <input type="text" name="phonenumber" required>
+
+               <!--FOR PASSWORD-->
+              
+               <label for="password"><b>Password</b></label>
+              <input type="password" name="password" required>
+
+              <!--FOR SUBMIT-->
+              
+              <input type="submit" name="create" value="Sign up">
+
           </div>
 
       </form>

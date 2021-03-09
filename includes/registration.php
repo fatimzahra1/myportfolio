efa076837e27a8dfcb786f37ae7b720dceb40e21
fatimzahra1 <fatimzahraelmadini@gmail.com>
2021-03-09 15:11:43 +0100

    --- Auto-Git Commit ---

--- includes/registration.php
+++ includes/registration.php
@@ -1,55 +1,71 @@
 <html>
   <head>
     <title>User registration</title>
+    <link rel="stylesheet"  href="../css/bootstrap.min.css">
   </head>
   <body>
 
   <div>
       <?php 
       if ( isset ($_POST['create'] ) ) {
-          echo 'User submitted.'; }
+          $firstname     =$_POST['firstname'];
+          $lastname      =$_POST['lastname'];
+          $email         =$_POST['email'];
+          $phonenumber   =$_POST['phonenumber'];
+          $password      =$_POST['password'];
+
+          echo 
         ?>
-  </div>
+  </div> $firstname " " $lastname  " "  $email " " $phonenumber " " $password
 
-  <div>
-      <form action="registration.php" method="post">
-          <div class="container">
-              <h1>Registration</h1>
-              <p>Fill in this form to registrate with us</p>
+ 
+   <div>
+     
+       <form action="registration.php" method="post">
+        
+            <div class="container">
 
-              <!--FOR FIRST NAME-->
+              <div class="row">
 
-              <label for="firstname"><b>First name</b></label>
-              <input type="text" name="firstname" required>
+                   <div class="col-sm-3"></div>
+                       <h1>Registration</h1>
+                       <p>Fill in this form to register with us</p>
+                        <hr class="mb-3">
+                        <!--FOR FIRST NAME-->
 
-               <!--FOR LAST NAME-->
+                        <label for="firstname"><b>First name</b></label>
+                        <input class="form-control" type="text" name="firstname" required>
+
+                         <!--FOR LAST NAME-->
               
-               <label for="lastname"><b>Last name</b></label>
-              <input type="text" name="lastname" required>
+                         <label for="lastname"><b>Last name</b></label>
+                         <input class="form-control" type="text" name="lastname" required>
 
-               <!--FOR EMAIL-->
+                         <!--FOR EMAIL-->
               
-               <label for="email"><b>Email adress</b></label>
-              <input type="email" name="email" required>
+                        <label for="email"><b>Email adress</b></label>
+                         <input class="form-control" type="email" name="email" required>
 
-               <!--FOR PHONE NUMBER-->
+                        <!--FOR PHONE NUMBER-->
               
-               <label for="phonenumber"><b>Phone number</b></label>
-              <input type="text" name="phonenumber" required>
+                          <label for="phonenumber"><b>Phone number</b></label>
+                          <input class="form-control" type="text" name="phonenumber" required>
 
-               <!--FOR PASSWORD-->
+                        <!--FOR PASSWORD-->
               
-               <label for="password"><b>Password</b></label>
-              <input type="password" name="password" required>
-
-              <!--FOR SUBMIT-->
+                         <label for="password"><b>Password</b></label>
+                         <input class="form-control" type="password" name="password" required>
+                         <hr class="mb-3">
+                        <!--FOR SUBMIT-->
               
-              <input type="submit" name="create" value="Sign up">
+                         <input class="btn btn-primary" type="submit" name="create" value="Sign up">
 
-          </div>
+                    </div>
+             </div>
+         </div>
 
-      </form>
-  </div>
+     </form>
+ </div>
 
-  </body>
+ </body>
 </html>
